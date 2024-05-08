83eb0ef7ecaa4231905d47b9d4f88e9179126363
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-05-08 16:41:51 -0500

    en clases

--- problema1/problema01/src/paquete01/Ejecutor.java
+++ problema1/problema01/src/paquete01/Ejecutor.java
@@ -7,13 +7,13 @@ public class Ejecutor {
     public static void main(String[] args) {
         LibretaNotas est1 = new LibretaNotas("Byron", 10, 9, 9.5f);
         LibretaNotas est2 = new LibretaNotas("Jeis", 8.75f, 9.8f);
-        
+
         est1.calcularPromedio();
         est2.calcularPromedio();
-        
+
         System.out.printf("%s\n", est1);
         System.out.printf("%s\n", est2);
-
+        
     }
 
 }
